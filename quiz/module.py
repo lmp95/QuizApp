6f476ce81cb94914bf6682e61f593a4cbceeb97e
lmp95 <lwinmgphyo95@gmail.com>
2021-05-25 20:00:01 +0630

    Added random quiz questions generate to math module

--- quiz/module.py
+++ quiz/module.py
@@ -1,14 +1,23 @@
-from flask import render_template
-from flask import Blueprint
-
-
-module_bp = Blueprint('module', __name__, template_folder='templates')
-@module_bp.route('/quiz/<module>')
-def get_module(module):
-    return render_template('quiz.html', keys=module, value=module)
+from flask import render_template, Blueprint, json
+import os, random
+from .model.question import Question
 
 
 index_bp = Blueprint('index', __name__, template_folder='templates')
 @index_bp.route('/quiz')
 def index():
     return render_template('index.html')
+
+
+module_bp = Blueprint('module', __name__, template_folder='templates')
+@module_bp.route('/quiz/<module>')
+def get_module(module):
+    questions = []
+    prj_root = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(prj_root, "data", "Math_Quiz.json")
+    data = json.load(open(json_url))
+    for obj in data:
+        questions.append(Question(**obj))
+    random.shuffle(questions)
+    questions = questions[:10]
+    return render_template('quiz.html', module=module, len=len(questions), Questions=questions)
